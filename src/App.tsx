3f1cf697fd98835e10636e0385adc68a1156c5ff
WadimSh <wadim11@yandex.ru>
2024-01-12 11:06:38 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Main from './components/main/main';
 import Footer from './components/footer/footer';
 
 function App() {
+  const print = 'print-sharik';
+
   return (
     <div className="App">
       <Header />
       <Main>
-        {'print-sharik'}
+        {print}
       </Main>
       <Footer />
     </div>
