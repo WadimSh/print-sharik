d68ecf3a5ed6db5f27848fbf8f75fba0388fac00
WadimSh <wadim11@yandex.ru>
2024-01-17 11:12:19 +0300

    feat

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
 
   return(
-    <footer></footer>
+    <footer>
+      &copy;&nbsp;2024&nbsp;г. АО&nbsp;&laquo;Европа&nbsp;уно&nbsp;трейд&raquo;
+    </footer>
   )
 };
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
 
   return(
-    <header></header>
+    <header>
+      <a href="#">logo</a>
+    </header>
   )
 };
 
