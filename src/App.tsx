7bf878686a37c5fcefed52ddb3e408f3e170e82d
WadimSh <wadim11@yandex.ru>
2024-01-18 13:31:21 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import { FC } from 'react';
 
 import Header from './components/header/header';
 import Main from './components/main/main';
 import Footer from './components/footer/footer';
 
-function App() {
+const App:FC = () => {
   const print = 'print-sharik';
 
   return (
