0f2e23e2f82526beaa57d4dbbbe38cc9b3b55c3d
WadimSh <wadim11@yandex.ru>
2024-01-26 10:18:08 +0300

    fix

--- src/components/sharik-constructor/sharik-constructor.tsx
+++ src/components/sharik-constructor/sharik-constructor.tsx
@@ -3,10 +3,11 @@ import { FC } from "react";
 import style from "./sharik-constructor.module.css";
 
 const SharikConstructor:FC = () => {
-
+  
   return (
     <section className={style.section}>
-
+      <div style={{ color: "#c9c9c9", paddingTop: "16px", textAlign: "center", width: "50%", height: "6%", borderBottom: "1px solid rgba(51, 38, 174, 0.08)" }}>Изображения</div>
+      <div style={{ paddingTop: "16px", textAlign: "center", width: "50%", height: "6%", borderBottom: "2px solid #396EC5" }}>Шарики</div>
     </section>
   )
 }
--- src/components/sharik-demo/sharik-demo.tsx
+++ src/components/sharik-demo/sharik-demo.tsx
@@ -1,12 +1,24 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import style from "./sharik-demo.module.css";
+import sharik from "../../assets/images/1102-0022_m1_1200.jpg";
+import shar from "../../assets/images/1102-0001_m1_1200.jpg";
 
 const SharikDemo:FC = () => {
+  const [logic, setLogic] = useState(false);
+
+  const handleCheckboxChange = () => {
+    const newCheckedValue = !logic;
+    setLogic(newCheckedValue);
+  };
 
   return (
     <section className={style.section}>
-      
+      <h1 style={{ margin: "0", paddingLeft: "50%" }}>Вы выбрали:</h1>
+      <div style={{ display: "flex", gap: "14px", marginTop: "34px" }}>
+        {!logic ? <img className={style.img} src={sharik} /> : <img className={style.img} src={shar} /> }
+        <input type="checkbox" checked={logic} onChange={handleCheckboxChange} />
+      </div>
     </section>
   )
 }
