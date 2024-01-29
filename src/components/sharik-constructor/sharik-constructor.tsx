a2055624b5bde132b464999c8a25914b1beb8d6f
WadimSh <wadim11@yandex.ru>
2024-01-29 11:06:03 +0300

    feat: изменено содержания основных компонентов приложения

--- src/components/sharik-constructor/sharik-constructor.tsx
+++ src/components/sharik-constructor/sharik-constructor.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 
 import style from "./sharik-constructor.module.css";
+import sharik from "../../assets/images/1102-0022_m1_1200.jpg";
 
 const SharikConstructor:FC = () => {
   
   return (
     <section className={style.section}>
-      
+      <img className={style.img} src={sharik} alt="sharik" />
     </section>
   )
 }
