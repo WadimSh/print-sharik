644d18ca721beaf8c54dad12075fb3707665d274
WadimSh <wadim11@yandex.ru>
2024-01-29 10:24:24 +0300

    fix

--- src/components/sharik-constructor/sharik-constructor.tsx
+++ src/components/sharik-constructor/sharik-constructor.tsx
@@ -6,8 +6,7 @@ const SharikConstructor:FC = () => {
   
   return (
     <section className={style.section}>
-      <div style={{ color: "#c9c9c9", paddingTop: "16px", textAlign: "center", width: "50%", height: "6%", borderBottom: "1px solid rgba(51, 38, 174, 0.08)" }}>Изображения</div>
-      <div style={{ paddingTop: "16px", textAlign: "center", width: "50%", height: "6%", borderBottom: "2px solid #396EC5" }}>Шарики</div>
+      
     </section>
   )
 }
