06953d3b83c8b536749af75057c3b4dcf0726be5
WadimSh <wadim11@yandex.ru>
2024-02-06 12:54:15 +0300

    feat: сверстан компонент header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from 'react-router-dom';
 
 import Logo from "../../ui/logo/logo";
+import { Layout } from "../../assets/icons/layout";
 
 import style from "./header.module.css";
 
 const Header = () => {
-
+  const [isActive, setIsActive] = useState(false);
+    
   return(
     <header className={style.wrapper}>
       <nav className={style.naviget}>
         <Logo />
-        <NavLink>
-
+        <NavLink to="/options" className={style.menu}>
+          <Layout />
+          <span>Мои варианты</span>
+          {isActive && <span className={style.point}></span>}
         </NavLink>
       </nav>
     </header>
