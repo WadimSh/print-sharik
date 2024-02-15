6b7e40f64f8436a913b8d723a1e8023f3d7db0d5
WadimSh <wadim11@yandex.ru>
2024-02-15 18:41:36 +0300

    fix

--- src/components/cards-block/cards-block.jsx
+++ src/components/cards-block/cards-block.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import data from "../../utils/data.json";
 import sortPrintsByCategory from "../../utils/sort-prints";
+import Dropdown from "../../ui/input/drop";
 
 const CardsBlock = () => {
 
@@ -8,7 +9,7 @@ const CardsBlock = () => {
 
   return (
     <>
-
+     <Dropdown />
     </>
   );
 };
