b68ec00b0e87f9abca25ce33fd33e7e73031d915
WadimSh <wadim11@yandex.ru>
2024-02-28 10:24:06 +0300

    fix: 28.02

---
+++ src/ui/title/title.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Title = ({ 
+  level,      
+  children,   
+  className, 
+  ...rest     
+}) => {
+  const validLevel = Math.min(level || 1, 6);
+  const Tag = `h${validLevel}`;
+  
+  return (
+    <Tag className={className} {...rest}>
+      {children}
+    </Tag>
+  );
+};
+
+export default Title;
